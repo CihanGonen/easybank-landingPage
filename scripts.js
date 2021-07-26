fa9832187ca6d082dee1cec75bf3dc558404c698
Cihan <cihangonen35@gmail.com>
2021-07-26 22:43:02 +0300

    project started from start navbar

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
-const navButton = document.getElementById('navbar-button');
-const navbarMenu = document.getElementById('navbar-menu');
-const openBtn = document.querySelector('.navbar__bars--hamburger');
-const closeBtn = document.querySelector('.navbar__bars--close');
-
-navButton.addEventListener('click',()=>{
-  openBtn.classList.toggle('is-active');
-  closeBtn.classList.toggle('is-active');
-  navbarMenu.classList.toggle('active');
+btnHamburger.addEventListener('click',()=>{
+  if(header.classList.contains('open')){
+    header.classList.remove('open')
+    overlay.classList.remove('fade-in');
+    overlay.classList.add('fade-out');
+  }else{
+    header.classList.add('open');
+    overlay.classList.remove('fade-out');
+    overlay.classList.add('fade-in');
+  }
 })
